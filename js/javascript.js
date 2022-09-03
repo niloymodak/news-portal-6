83e2e151319da3a855187d32be894859ddc58812
niloymodak <niloymodak5@gmail.com>
2022-09-03 09:05:10 +0600

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+const loadData = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+
+loadData();
