ac31685b4c96fe20efc7ec917572eab8914df23a
niloymodak <niloymodak5@gmail.com>
2022-09-03 16:02:30 +0600

    second commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,31 @@
-const loadData = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-        .then(res => res.json())
-        .then(data => console.log(data))
+const loadNews = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const news = await res.json();
+    displayNews(news.data.news_category);
+
 }
 
+const displayNews = allNews => {
+    const newsContainer = document.getElementById('news-container');
+    allNews.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+        <div class="col-md-4">
+                 <img src="" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">${news.category_name}</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                         additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        </div>
+        `;
+        newsContainer.appendChild(newsDiv);
+    })
 
-loadData();
+}
+loadNews();
