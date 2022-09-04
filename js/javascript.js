492ed88ec5bb461eda64ef251251c2dbf732880e
niloymodak <niloymodak5@gmail.com>
2022-09-04 11:14:09 +0600

    fourth commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,18 @@
 const loadCategories = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const url = `https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
+    const categories = await res.json();
+    displayNews(categories.data.news_category);
 
 }
-// const loadNewsCategories = async () => {
-//     const url = `https://openapi.programming-hero.com/api/news/category/01`
-//     const res = await fetch(url);
-//     const newsCategory = await res.json();
-//     console.log(newsCategory.data);
+const loadNewsCategories = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const res = await fetch(url);
+    const newsCategory = await res.json();
+    console.log(newsCategory.data);
 
-// }
-// loadNewsCategories();
+}
+loadNewsCategories();
 
 const displayNews = allNews => {
     const newsContainer = document.getElementById('news-container');
@@ -25,7 +25,7 @@ const displayNews = allNews => {
         </div>
         <div class="col-md-8">
         <div class="card-body">
-            <h5 class="card-title"></h5>
+            <h5 class="card-title">${news.category_name}</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
                          additional content. This content is a little bit longer.</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
