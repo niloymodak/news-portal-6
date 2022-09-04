356d8d5dfa82244326b1288fe234fd52d6e3dac9
niloymodak <niloymodak5@gmail.com>
2022-09-04 11:08:07 +0600

    third commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,18 @@
-const loadNews = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/categories`
+const loadCategories = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
     const res = await fetch(url);
-    const news = await res.json();
-    displayNews(news.data.news_category);
+    const data = await res.json();
+    console.log(data);
 
 }
+// const loadNewsCategories = async () => {
+//     const url = `https://openapi.programming-hero.com/api/news/category/01`
+//     const res = await fetch(url);
+//     const newsCategory = await res.json();
+//     console.log(newsCategory.data);
+
+// }
+// loadNewsCategories();
 
 const displayNews = allNews => {
     const newsContainer = document.getElementById('news-container');
@@ -17,7 +25,7 @@ const displayNews = allNews => {
         </div>
         <div class="col-md-8">
         <div class="card-body">
-            <h5 class="card-title">${news.category_name}</h5>
+            <h5 class="card-title"></h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
                          additional content. This content is a little bit longer.</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
@@ -28,4 +36,4 @@ const displayNews = allNews => {
     })
 
 }
-loadNews();
+loadCategories();
